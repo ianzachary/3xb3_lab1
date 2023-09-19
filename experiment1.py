fcff7f68d1685e5f0fbbd7cd931ac81dc3ba6813
hatoumg <ghena.ali.hatoum2003@gmail.com>
2023-09-19 16:14:29 -0400

    Improved and finished insertion2 and experiment1

--- experiment1.py
+++ experiment1.py
@@ -1,11 +1,12 @@
 import timeit
-import random
 import matplotlib.pyplot as plot
 from bad_sorts import create_random_list
 from bad_sorts import insertion_sort
 from bad_sorts import selection_sort
+from bad_sorts import bubble_sort
 
-def test_insertion(n, m, step):
+
+def test_time(n, m, step, function):
     times = []
     list_sizes = []
     for i in range(0, n, step):
@@ -13,15 +14,20 @@ def test_insertion(n, m, step):
         for _ in range(m):
             L = create_random_list(i, i)
             start = timeit.default_timer()
-            L2 = selection_sort(L)
+            L2 = function(L)
             end = timeit.default_timer()
             time += end - start
         times.append(time/m)
         list_sizes.append(i)
     return list_sizes, times
 
-list_sizes, times = test_insertion(1000, 10, 50)
-plot.plot(list_sizes, times)
+list_sizes_s, times_s = test_time(1000, 10, 50, selection_sort)
+list_sizes_i, times_i = test_time(1000, 10, 50, insertion_sort)
+list_sizes_b, times_b = test_time(1000, 10, 50, bubble_sort)
+plot.plot(list_sizes_s, times_s)
+plot.plot(list_sizes_i, times_i)
+plot.plot(list_sizes_b, times_b)
+plot.legend(["Selection Sort","Insertion Sort","Bubble Sort"])
 plot.xlabel("List Sizes")
 plot.ylabel("Time (s)")
 plot.show()
