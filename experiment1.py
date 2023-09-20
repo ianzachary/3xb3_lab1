14164aed1e762e495f7480a1769422dd220510f5
hatoumg <ghena.ali.hatoum2003@gmail.com>
2023-09-20 08:50:13 -0400

    Simplifed insertion2 and experiment1

--- experiment1.py
+++ experiment1.py
@@ -6,7 +6,7 @@ from bad_sorts import selection_sort
 from bad_sorts import bubble_sort
 
 
-def test_time(n, m, step, function):
+def test_time(function, n = 1000, m = 10, step = 50):
     times = []
     list_sizes = []
     for i in range(0, n, step):
@@ -21,14 +21,18 @@ def test_time(n, m, step, function):
         list_sizes.append(i)
     return list_sizes, times
 
-list_sizes_s, times_s = test_time(1000, 10, 50, selection_sort)
-list_sizes_i, times_i = test_time(1000, 10, 50, insertion_sort)
-list_sizes_b, times_b = test_time(1000, 10, 50, bubble_sort)
-plot.plot(list_sizes_s, times_s)
-plot.plot(list_sizes_i, times_i)
-plot.plot(list_sizes_b, times_b)
-plot.legend(["Selection Sort","Insertion Sort","Bubble Sort"])
-plot.xlabel("List Sizes")
-plot.ylabel("Time (s)")
-plot.show()
-#print(time)
+
+def main():
+    list_sizes_s, times_s = test_time(selection_sort)
+    list_sizes_i, times_i = test_time(insertion_sort)
+    list_sizes_b, times_b = test_time(bubble_sort)
+    plot.plot(list_sizes_s, times_s)
+    plot.plot(list_sizes_i, times_i)
+    plot.plot(list_sizes_b, times_b)
+    plot.legend(["Selection Sort","Insertion Sort","Bubble Sort"])
+    plot.xlabel("List Sizes")
+    plot.ylabel("Time (s)")
+    plot.show()
+
+if __name__ == "__main__":
+    main()
