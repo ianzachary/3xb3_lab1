2f93f29d1929bc82a15bcd84aef4162bbb5ffc66
iqraawan12 <iqra.awan10@gmail.com>
2023-09-20 19:37:29 -0400

    selection sort for experiement2

---
+++ selection2.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plot
+from bad_sorts import selection_sort, swap 
+from experiment1 import test_time
+
+def selection_sort2(L):
+    i = 0
+    k = len(L) - 1
+    while i < k:
+        min_index, max_index = findMaxMin_index(L, i, k)
+        if i == max_index:
+            max_index = min_index
+        elif k == i:
+            k = min_index
+        swap(L, i, min_index)
+        swap(L, k, max_index)
+        i += 1
+        k -= 1
+    return L
+
+def findMaxMin_index(L, n, k):
+    min_index = n
+    max_index = n
+    for i in range(n+1, k+1):
+        if L[i] < L[min_index]:
+            min_index = i
+        elif L[i] > L[max_index]:
+            max_index = i
+    return min_index, max_index
+
+print(selection_sort2([2,10,3,0,7,5,9]))
+
+list_sizes1, times1 = test_time(selection_sort)
+list_sizes2, times2 = test_time(selection_sort2)
+
+plot.plot(list_sizes1, times1)
+plot.plot(list_sizes2, times2)
+plot.legend(["Selection 1", "Selection 2"])
+plot.xlabel("List Sizes")
+plot.ylabel("Time (s)")
+plot.show()
