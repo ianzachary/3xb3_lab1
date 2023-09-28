419c50be97a581d9f82e0541c7402328ffa5099c
hatoumg <ghena.ali.hatoum2003@gmail.com>
2023-09-28 10:52:35 -0400

    Fixed experiement 5

--- experiment4.py
+++ experiment4.py
@@ -5,9 +5,9 @@ from experiment1 import test_time
 import matplotlib.pyplot as plot
 
 def main():
-    list_sizes_s, times_s = test_time(quicksort)
-    list_sizes_i, times_i = test_time(mergesort)
-    list_sizes_b, times_b = test_time(heapsort)
+    list_sizes_s, times_s = test_time(quicksort, m=500)
+    list_sizes_i, times_i = test_time(mergesort,  m=500)
+    list_sizes_b, times_b = test_time(heapsort,  m=500)
     plot.plot(list_sizes_s, times_s)
     plot.plot(list_sizes_i, times_i)
     plot.plot(list_sizes_b, times_b)
--- experiment5.py
+++ experiment5.py
@@ -1,12 +1,20 @@
 from good_sorts import quicksort
+from good_sorts import mergesort
+from good_sorts import heapsort
 from experiment3 import test_near_sorted_list
 import matplotlib.pyplot as plot
 
 def main():
-    list_sizes_s, times_s = test_near_sorted_list(quicksort)
+    list_sizes_s, times_s = test_near_sorted_list(quicksort, n = 950)
+    list_sizes_m, times_m = test_near_sorted_list(mergesort, n = 950)
+    list_sizes_h, times_h = test_near_sorted_list(heapsort, n = 950)
+
     plot.plot(list_sizes_s, times_s)
-    plot.legend(["Quick Sort"])
-    plot.xlabel("List Sizes")
+    plot.plot(list_sizes_m, times_m)
+    plot.plot(list_sizes_h, times_h)
+
+    plot.legend(["Quick Sort", "Merge Sort", "Heap Sort"])
+    plot.xlabel("Swaps")
     plot.ylabel("Time (s)")
     plot.show()
 
